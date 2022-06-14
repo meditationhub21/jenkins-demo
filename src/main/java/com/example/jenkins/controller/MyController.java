bd595527be3b16515148357304ed4e93990b6dda
Shashank Nema <meditationhub21@gmail.com>
2022-06-14 18:09:42 +0530

    Added Service layer.

--- src/main/java/com/example/jenkins/controller/MyController.java
+++ src/main/java/com/example/jenkins/controller/MyController.java
@@ -1,15 +1,19 @@
 package com.example.jenkins.controller;
 
+import com.example.jenkins.service.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
+    @Autowired
+    private MyService myService;
+
     @GetMapping("/get")
     public String getDetails(){
 
-        return "Hello!!!";
-
+        return myService.get();
     }
 }
---
+++ src/main/java/com/example/jenkins/service/MyService.java
@@ -0,0 +1,11 @@
+package com.example.jenkins.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+    public String get(){
+        return "Hello world!!!";
+    }
+}
